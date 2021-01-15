6235755c6b6bf5e03910d95df8ac67f4131b5aeb
Hopin-fing <dyggalag@gmail.com>
2021-01-15 19:15:48 +0300

    version 0.7

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import socket from '../socket';
 
 function Chat({ users, messages, userName, roomId, onAddMessage }) {
-    // const [messageValue, setMessageValue] = React.useState('');
-    // const messagesRef = React.useRef(null);
-    //
-    // const onSendMessage = () => {
-    //     socket.emit('ROOM:NEW_MESSAGE', {
-    //         userName,
-    //         roomId,
-    //         text: messageValue,
-    //     });
-    //     onAddMessage({ userName, text: messageValue });
-    //     setMessageValue('');
-    // };
-    //
-    // React.useEffect(() => {
-    //     messagesRef.current.scrollTo(0, 99999);
-    // }, [messages]);
+    const [messageValue, setMessageValue] = React.useState('');
+    const messagesRef = React.useRef(null);
+
+    const onSendMessage = () => {
+        socket.emit('ROOM:NEW_MESSAGE', {
+            userName,
+            roomId,
+            text: messageValue,
+        });
+        onAddMessage({ userName, text: messageValue });
+        setMessageValue('');
+    };
+
+    React.useEffect(() => {
+        messagesRef.current.scrollTo(0, 99999);
+    }, [messages]);
 
     return (
         <div className="chat">
@@ -32,27 +32,27 @@ function Chat({ users, messages, userName, roomId, onAddMessage }) {
                 </ul>
             </div>
             <div className="chat-messages">
-                {/*<div ref={messagesRef} className="messages">*/}
-                {/*    {messages.map((message) => (*/}
-                {/*        <div className="message">*/}
-                {/*            <p>{message.text}</p>*/}
-                {/*            <div>*/}
-                {/*                <span>{message.userName}</span>*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-                {/*    ))}*/}
-                {/*</div>*/}
-                {/*<form>*/}
-                {/*  <textarea*/}
-                {/*      value={messageValue}*/}
-                {/*      onChange={(e) => setMessageValue(e.target.value)}*/}
-                {/*      className="form-control"*/}
-                {/*      rows="3">*/}
-                {/*  </textarea>*/}
-                {/*    <button onClick={onSendMessage} type="button" className="btn btn-primary">*/}
-                {/*        Отправить*/}
-                {/*    </button>*/}
-                {/*</form>*/}
+                <div ref={messagesRef} className="messages">
+                    {messages.map((message) => (
+                        <div className="message">
+                            <p>{message.text}</p>
+                            <div>
+                                <span>{message.userName}</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <form>
+                  <textarea
+                      value={messageValue}
+                      onChange={(e) => setMessageValue(e.target.value)}
+                      className="form-control"
+                      rows="3">
+                  </textarea>
+                    <button onClick={onSendMessage} type="button" className="btn btn-primary">
+                        Отправить
+                    </button>
+                </form>
             </div>
         </div>
     );
